078dfa27453cbb5d40883dabd51ce03cb7f7c029
Shivamyadav48 <shivam.2022ug1048@iiitranchi.ac.in>
2025-04-29 19:52:49 +0530

    update

--- lib/shared/utils/validator.dart
+++ lib/shared/utils/validator.dart
@@ -1,14 +1,14 @@
 class Validators {
   static String? validateNoteTitle(String? value) {
     if (value == null || value.trim().isEmpty) {
-      return 'Title cannot be empty';
+      return 'Title can not be empty';
     }
     return null;
   }
 
   static String? validateNoteContent(String? value) {
     if (value == null || value.trim().isEmpty) {
-      return 'Note content cannot be empty';
+      return 'Note content can not be empty';
     }
     return null;
   }
