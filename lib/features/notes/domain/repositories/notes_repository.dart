749690dacfa6e4bcdd32030bc5355f4ca0cc6d7f
Shivamyadav48 <shivam.2022ug1048@iiitranchi.ac.in>
2025-06-04 18:40:36 +0530

    update

--- lib/features/notes/domain/repositories/notes_repository.dart
+++ lib/features/notes/domain/repositories/notes_repository.dart
@@ -2,7 +2,10 @@ import '../entities/note.dart';
 
 abstract class NotesRepository {
   Future<List<Note>> getNotes();
+
   Future<void> createNote(String title, String content);
+
   Future<void> updateNote(String id, String title, String content);
+
   Future<void> deleteNote(String id);
 }
