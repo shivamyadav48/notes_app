18e0f7368d7d42a721a5c00e96f8fa0b7d0009b6
Shivamyadav48 <shivam.2022ug1048@iiitranchi.ac.in>
2025-06-04 18:41:12 +0530

    update

--- lib/features/notes/data/datasources/drive_remote_data_source.dart
+++ lib/features/notes/data/datasources/drive_remote_data_source.dart
@@ -100,7 +100,8 @@ class DriveNotesRemoteDataSource {
   }
 
   /// Update existing note by fileId
-  Future<void> updateNote(String fileId, String title, String newContent) async {
+  Future<void> updateNote(
+      String fileId, String title, String newContent) async {
     final driveApi = await _getDriveApi();
     final file = drive.File()
       ..name = title; // This sets the title/name of the file
